a5531da5003f400340b8803ea8e464601a3ce4cb
YukkuriC <799433638@qq.com>
2025-05-14 00:25:56 +0800

    player attacker & collect orbs

--- server_scripts/contents/all_right_click.js
+++ server_scripts/contents/all_right_click.js
@@ -8,14 +8,24 @@ let potUseFuncs = {
         plr.abilities.flyingSpeed = 0.15
         plr.onUpdateAbilities()
     },
-    potted_dandelion(lvl, pos) {
+    potted_dandelion(lvl, pos, plr) {
         let tryChest = lvl.getBlock(pos.above())?.inventory
         let range = AABB.of(pos.x - 100, -320, pos.z - 100, pos.x + 101, 320, pos.z + 101)
+        let dmgSrc = plr.damageSources().playerAttack(plr)
         for (let e of lvl.getEntitiesWithin(range)) {
             if (!e.isMonster()) continue
             e.setPosition(pos.x + 0.5, pos.y + 1, pos.z + 0.5)
+            e.attack(dmgSrc, 1)
             e.kill()
         }
+        let orbTotalExp = 0
+        for (let e of lvl.getEntitiesWithin(range)) {
+            if (e.type != 'minecraft:experience_orb') continue
+            let { Count, Value } = e.nbt
+            orbTotalExp += Count * Value
+            e.discard()
+        }
+        plr.giveExperiencePoints(orbTotalExp)
         if (tryChest)
             for (let e of lvl.getEntitiesWithin(range)) {
                 if (e.type != 'minecraft:item') continue
