4f0648997d3dd9630feeac8d49ccf9d75f51e8c5
YukkuriC <799433638@qq.com>
2025-05-31 14:51:32 +0800

    摆烂了，紫水晶也挖挖

--- server_scripts/compat/ore_tags.js
+++ server_scripts/compat/ore_tags.js
@@ -1,4 +1,5 @@
 ServerEvents.tags('block', e => {
+    e.add('forge:ores', 'amethyst_block')
     if (Platform.isLoaded('create')) e.add('forge:ores', 'andesite')
     if (Platform.isLoaded('irons_spellbooks')) e.add('forge:ores', 'irons_spellbooks:arcane_debris')
 })
